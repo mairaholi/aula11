470c75e108f723a8a63ddb280c488e7084b3540b
Maíra Heloísa Silva Oliveira <heloisa3031@gmail.com>
2022-06-15 16:04:53 -0300

    Adicionada a classe Quadrado.

---
+++ FormasApp/Abstractions/Quadrado.cs
@@ -0,0 +1,11 @@
+﻿
+namespace FormasApp.Abstractions
+{
+    public class Quadrado : Forma
+    {
+        public override string PrintMe()
+        {
+            return "Quadrado";
+        }
+    }
+}
